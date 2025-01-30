4b1469483c3ee71bca5fa84ee92c1e0cc8516bb1
envyst <nurardli.saputra@gmail.com>
2025-01-30 23:51:00 +0700

    update

--- dockerization/run_gaianet_node.sh
+++ dockerization/run_gaianet_node.sh
@@ -3,22 +3,9 @@
 sudo chmod +x install_docker.sh
 ./install_docker.sh
 docker compose up -d
-# Wait for a specific log entry from the main container
-CONTAINER_NAME="gaianet_node"  # Change this to your actual container name
-SUCCESS_MESSAGE="Startup complete"    # Change this to the expected log message
 
-echo "Waiting for $CONTAINER_NAME to complete startup..."
-
-while true; do
-    if docker logs "$CONTAINER_NAME" 2>&1 | grep -q "$SUCCESS_MESSAGE"; then
-        echo "Docker entrypoint script finished!"
-        break
-    fi
-    sleep 2  # Check every 2 seconds
-done
-
-# Run the next command
-echo "Done"
+docker exec gaianet_node /root/gaianet/bin/gaianet init
+docker exec gaianet_node /root/gaianet/bin/gaianet start
 
 # Extract Node ID
 node_id=$(docker exec gaianet_node jq -r '.address' nodeid.json)
@@ -36,3 +23,4 @@ echo "screen -S gaianet-node-throughput"
 echo "cd ../go-app/"
 echo "./program -nodeID $node_id"
 echo "Ctrl + A + D to detach from the screen session"
+echo "------------------------------------------------------------"
